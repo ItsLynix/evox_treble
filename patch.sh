92d39d288f37e46b2e46c6d1e4236a87831781c7
ItsLynix <71310187+ItsLynix@users.noreply.github.com>
2022-09-27 16:35:01 +0200

    Name change to fix an issue

--- patch.sh
+++
@@ -1,2 +0,0 @@
-# This script currently has no use as I'm still experimenting with creating an efficient patch application script.
-echo "This script currently has no use as I'm still experimenting with creating an efficient patch application script."
---
+++ patching.sh
@@ -0,0 +1,2 @@
+# This script currently has no use as I'm still experimenting with creating an efficient patch application script.
+echo "This script currently has no use as I'm still experimenting with creating an efficient patch application script."
